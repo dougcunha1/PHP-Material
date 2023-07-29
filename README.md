0226468cfd975c6dd7dbcfa6f373bec9035635fc
dougcunha1 <dougcunha@academico.ufs.br>
2023-07-29 17:21:57 -0300

    Material

--- README.md
+++ README.md
@@ -212,3 +212,41 @@ No exemplo abaixo foram utilizados for e while loop.
     }
 ?>
 ````
+
+## Arrays em PHP
+
+````php
+<?php
+    // Array de strings
+    $comidas = array("Maçã", "Banana", "Laranja");
+    echo $comidas;
+?>
+````
+
+### Percorrendo um array utilizando foreach
+
+````php
+<?php
+    // Array de strings
+    $comidas = array("Melância", "Maçã", "Banana", "Laranja");
+
+    // A função array_push(nome_array, valor) adiciona um valor no final do array
+    array_push($comidas, "Caju");
+
+    // A função array_pop(nome_array) remove o último elemento do array
+    array_pop($comidas);
+
+    // A função array_shift(nome_array) remove o primeiro elemento do array
+    array_shift($comidas);
+
+    // A função array_reverse(nome_array) reverte a ordem do array e retorna um novo array
+    $comidas_contrario = array_reverse($comidas);
+    
+    foreach ($comidas_contrario as $comida) {
+        echo $comida . "<br>";
+    }
+
+    // A função count(nome_array) retorna o número de elementos do array
+    echo "Total de comidas: " . count($comidas) . "<br>"
+?>
+````
--- index.php
+++ index.php
@@ -1,41 +1,23 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form action="index.php" method="post">
-        <label for="">Digite um número:</label>
-        <input type="number" name="numero"><br>
-        <input type="submit" value="Enviar">
-    </form>
-</body>
-</html>
-
 <?php
-    // Armazena o valor do campo numero
-    $contador = $_POST["numero"];
-    $rodando = false;
-    // Exibe o resultado
-    for ($i = 1; $i <= $contador; $i++) {
-        echo $i . "<br>";
+    // Array de strings
+    $comidas = array("Melância", "Maçã", "Banana", "Laranja");
 
-        if ($i == $contador) {
-            $rodando = true;
-        }
+    // A função array_push(nome_array, valor) adiciona um valor no final do array
+    array_push($comidas, "Caju");
+
+    // A função array_pop(nome_array) remove o último elemento do array
+    array_pop($comidas);
+
+    // A função array_shift(nome_array) remove o primeiro elemento do array
+    array_shift($comidas);
+
+    // A função array_reverse(nome_array) reverte a ordem do array e retorna um novo array
+    $comidas_contrario = array_reverse($comidas);
+
+    foreach ($comidas_contrario as $comida) {
+        echo $comida . "<br>";
     }
 
-    // Exemplo com while loop
-    $num = 10;
-
-    echo "<br>";
-
-    if ($rodando) {
-        while ($num > 0) {
-            echo $num . "<br>";
-            $num--;
-        }
-    }
+    // A função count(nome_array) retorna o número de elementos do array
+    echo "Total de comidas: " . count($comidas) . "<br>"
 ?>
