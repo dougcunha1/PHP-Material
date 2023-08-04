17152a31f4f77495989d03257630ec3083b40574
dougcunha1 <dougcunha@academico.ufs.br>
2023-08-04 08:23:11 -0300

    Material

--- index.php
+++ index.php
@@ -1,17 +1,24 @@
 <?php
     include("database.php");
-?>
 
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    Hello<br>
-</body>
-</html>
+    // Duas variáveis quaisquer
+    $username = "Douglas";
+    $password = "universe123";
+    // Aplicando uma hash na senha utilizando o algoritmo bcrypt
+    $hash = password_hash($password, PASSWORD_DEFAULT);
+
+    // Query simplse que seleciona a tabela users e insere dados
+    $sql = "INSERT INTO users(user, password) VALUES('$username', '$hash')";
+
+    try {
+        // A função mysqli_query recebe dois parâmetros, uma conexão com o db e uma query
+        mysqli_query($conn, $sql);
+        echo "User is now registered!";
+    }
+    catch (mysqli_sql_exception) {
+        echo "Could not register user!";
+    }
+
+    // Fecha a conexão com o banco de dados
+    mysqli_close($conn);
+?>
