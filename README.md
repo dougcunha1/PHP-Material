f3be33e2d7f58aba8db33fe041b2d0c12895826f
dougcunha1 <dougcunha@academico.ufs.br>
2023-08-04 10:10:21 -0300

    Material

--- README.md
+++ README.md
@@ -1325,3 +1325,71 @@ Outro exemplo envolvendo a consulta de dados no banco de dados
     mysqli_close($conn);
 ?>
 ```
+
+## Criando um formulário de registro
+
+```php
+<?php
+    // Inclui uma cópia do nosso arquivo database.php
+    include("database.php");
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>User Registration</title>
+</head>
+<body>
+    <!-- Script PHP que previne Cross-Site Scripting utilizando o filter htmlspecialchars -->
+    <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="POST">
+        <h2>Welcome to Fakebook!</h2>
+        <label>Username:</label>
+        <input type="text" name="username">
+        <label>Password:</label>
+        <input type="password" name="password">
+        <input type="submit" name="submit" value="Register">
+    </form>
+</body>
+</html>
+
+<?php
+    // Analisa para ver se o método de requisição é POST
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Aplica um filtro no username para previnir scripts maliciosos
+        $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
+        $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_SPECIAL_CHARS);
+
+        // Checando se o username está vazio
+        if (empty($username)) {
+            echo "Please enter an username!";
+        }
+        elseif (empty($password)) {
+            echo "Please enter an password!";
+        }
+        else {
+            // Aplica um algoritmo(BCrypt) de hash no password
+            $hash = password_hash($password, PASSWORD_DEFAULT);
+
+            // Query SQL que insere dados na tabela users
+            $sql = "INSERT INTO users(user, password) VALUES('$username', '$hash')";
+
+            try {
+                // Aplicando a query utilizando a função mysqli_query()
+                mysqli_query($conn, $sql);
+                // Informa ao usuário que o mesmo se registrou com sucesso
+                echo "You are now registered!";
+            }
+            catch (mysqli_sql_exception) {
+                echo "That username is taken!";
+            }
+        }
+    }
+
+    // Fecha a conexão com o banco de dados
+    mysqli_close($conn);
+?>
+```
--- index.php
+++ index.php
@@ -1,24 +1,63 @@
 <?php
+    // Inclui uma cópia do nosso arquivo database.php
     include("database.php");
+?>
 
-    $sql = "SELECT * FROM users";
-    // Result é basicamente um "objeto"
-    $result = mysqli_query($conn, $sql);
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>User Registration</title>
+</head>
+<body>
+    <!-- Script PHP que previne Cross-Site Scripting utilizando o filter htmlspecialchars -->
+    <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="POST">
+        <h2>Welcome to Fakebook!</h2>
+        <label>Username:</label>
+        <input type="text" name="username">
+        <label>Password:</label>
+        <input type="password" name="password">
+        <input type="submit" name="submit" value="Register">
+    </form>
+</body>
+</html>
 
-    // A função mysqli_num_rows calcula a quantidade de linhas existentes no db
-    if (mysqli_num_rows($result) > 0) {
-        // A função mysqli_fetch_assoc retorna a próxima linha disponível
-        while($row = mysqli_fetch_assoc($result)) {
-            echo $row["id"] . "<br>";
-            echo $row["user"] . "<br>";
-            echo $row["reg_date"] . "<br>";
-            echo "<br>";
+<?php
+    // Analisa para ver se o método de requisição é POST
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Aplica um filtro no username para previnir scripts maliciosos
+        $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
+        $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_SPECIAL_CHARS);
+
+        // Checando se o username está vazio
+        if (empty($username)) {
+            echo "Please enter an username!";
         }
+        elseif (empty($password)) {
+            echo "Please enter an password!";
+        }
+        else {
+            // Aplica um algoritmo(BCrypt) de hash no password
+            $hash = password_hash($password, PASSWORD_DEFAULT);
 
-    }
-    else {
-        echo "No user found!";
+            // Query SQL que insere dados na tabela users
+            $sql = "INSERT INTO users(user, password) VALUES('$username', '$hash')";
+
+            try {
+                // Aplicando a query utilizando a função mysqli_query()
+                mysqli_query($conn, $sql);
+                // Informa ao usuário que o mesmo se registrou com sucesso
+                echo "You are now registered!";
+            }
+            catch (mysqli_sql_exception) {
+                echo "That username is taken!";
+            }
+        }
     }
 
+    // Fecha a conexão com o banco de dados
     mysqli_close($conn);
 ?>
